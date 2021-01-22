6a8450aaadf3560dc247d9a872f2fcc2f1a03714
YeeClaw <austin@jcolt.net>
2021-01-22 13:12:04 -0700

    Add start to Employee class

--- misc/src/com/coltaj/miscwork/lab5/Employee.java
+++ misc/src/com/coltaj/miscwork/lab5/Employee.java
@@ -2,5 +2,26 @@ package com.coltaj.miscwork.lab5;
 
 public class Employee extends Person{
 
+    private double payRate, hoursWorked;
+    private String department;
 
+    public final int HOURS = 40;
+    public final double OVERTIME = 1.5;
+
+    public Employee() {
+
+        super();
+    }
+
+    public Employee(String firstName, String lastName, double payRate, double hoursWorked, String department) {
+
+        super(firstName, lastName);
+        this.payRate = payRate;
+        this.department = department;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public String toString() {
+        return "The wages for " + super.getFirstName() + " from the " + department + " department are: $"; // ! Finish later
+    }
 }
